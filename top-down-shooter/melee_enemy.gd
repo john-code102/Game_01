f6a0d076be6da487821967455f4ae7585b05fd12
nero <cheesebugga783@gmail.com>
2025-09-16 22:33:02 +1200

    Update melee_enemy.gd

--- top-down-shooter/melee_enemy.gd
+++ top-down-shooter/melee_enemy.gd
@@ -6,12 +6,13 @@ var JUMP_VELOCITY = -400.0
 @export var player : Node2D
 var direction : Vector2
 
-func _init() -> void:
-	player = Global.Player
 
 
 
 func _physics_process(_delta: float) -> void:
+
+	if player == null and Global.Player != null:
+		player = Global.Player
 
 	#direction is the direction towards player
 	if global_position.distance_to(player.global_position) < 15 :
