8a6cdd27ff153bcfa7d70ac8e9876ab3d34b49ae
Stream <115670208+Pokeboy4yt@users.noreply.github.com>
2025-09-16 21:58:56 +1200

    a

--- top-down-shooter/global.gd
+++ top-down-shooter/global.gd
@@ -5,3 +5,8 @@ var controller = false
 var Player
 
 var gun : PackedScene
+
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("return home"):
+		get_tree().change_scene_to_file("res://menu.tscn")
